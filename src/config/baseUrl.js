a91ddf3e05e0a27b28b9d3fdebc2d76596aa0663
Israfil-Hossain <Israfil166091@gmail.com>
2023-04-11 22:47:51 +0600

    Update baseUrl

--- src/config/baseUrl.js
+++ src/config/baseUrl.js
@@ -1,9 +1,11 @@
+export const baseUrl = {
+    information: "http://89.116.228.42:4060/api",
+    file: "http://89.116.228.42:4060/api",
+};
+
 // export const baseUrl = {
-//     information: "http://89.116.228.42:4060/api",
-//     file: "http://89.116.228.42:4060/api",
+//     information: "http://localhost:4060/api",
+//     file: "http://localhost:4060/api",
 // };
 
-export const baseUrl = {
-    information: "http://localhost:4060/api",
-    file: "http://localhost:4060/api",
-};
+// mongoDb : mongodb+srv://pngcrown:pngcrown@cluster0.bdexcro.mongodb.net/pngCrown?retryWrites=true&w=majority
