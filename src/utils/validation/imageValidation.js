877ceea1fefff10857d764c2cbd73a8c7b7b1c49
Israfil-Hossain <Israfil166091@gmail.com>
2023-05-01 01:08:32 +0600

    Image Size max 5 mb

--- src/utils/validation/imageValidation.js
+++ src/utils/validation/imageValidation.js
@@ -3,8 +3,8 @@ import * as Yup from "yup";
 const imageValidationSchema = Yup.object().shape({
   image: Yup.mixed()
     .required("Image is required")
-    .test("fileSize", "Image size should be less than 1 MB", (value) => {
-      return value ? value.size <= 1024 * 1024 : true;
+    .test("fileSize", "Image size should be less than 5 MB", (value) => {
+      return value ? value.size <= 5 * 1024 * 1024 : true;
     })
     .test("fileType", "Only PNG images are allowed", (value) => {
       return value ? value.type === "image/png" : true;
